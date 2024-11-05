31cf06c24784f0f9f9c419478e7e5a512b419a9f
yootal <khyoo96@naver.com>
2024-11-05 09:34:29 +0900

    1105 SpringSecurity Pro JWT study2

---
+++ StudySecurityProJWT/src/main/java/security/pro/jwt/studysecurityprojwt/StudySecurityProJwtApplication.java
@@ -0,0 +1,12 @@
+package security.pro.jwt.studysecurityprojwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySecurityProJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySecurityProJwtApplication.class, args);
+    }
+}
